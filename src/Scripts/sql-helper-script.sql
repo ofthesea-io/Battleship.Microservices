437c28bca88c2c41d1d72f3cf07a1786e685978a
Jakes Potgieter <jakespotgieter@gmail.com>
2020-06-10 18:24:17 +0100

    Feature - Docker continued

--- src/Scripts/sql-helper-script.sql
+++
@@ -1,7 +0,0 @@
-SELECT  p.PlayerId, p.Firstname, p.Lastname, P.Email, p.IsDemo, p.DateCreated from [Battleship.Player].dbo.Player p
-SELECT * FROM [Battleship.Game].dbo.GamePlay
-SELECT * FROM [Battleship.ScoreCard].dbo.PlayerScoreCard psc 
-
-DELETE FROM [Battleship.Game].dbo.GamePlay
-DELETE FROM [Battleship.ScoreCard].dbo.PlayerScoreCard
-DELETE FROM [Battleship.Player].dbo.Player WHERE [Battleship.Player].dbo.Player.IsDemo = 0
